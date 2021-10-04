9a1f74a41ce75a57ea6bc816c3a8c51156a48db8
김재정 <jaejeong.kim@kbfg.com>
2021-10-04 16:17:01 +0900

    bentoml test 미완성

--- BentoML/main.py
+++ BentoML/main.py
@@ -1,6 +1,8 @@
 # 1. 모델 학습 코드
 from sklearn import svm
 from sklearn import datasets
+# import the IrisClassifier class defined above
+from iris_classifier import IrisClassifier
 
 # Load training data
 iris = datasets.load_iris()
@@ -10,9 +12,6 @@ X, y = iris.data, iris.target
 clf = svm.SVC(gamma='scale')
 clf.fit(X, y)
 
-# import the IrisClassifier class defined above
-from iris_classifier import IrisClassifier
-
 # Create a iris classifier service instance
 iris_classifier_service = IrisClassifier()
 
@@ -20,4 +19,11 @@ iris_classifier_service = IrisClassifier()
 iris_classifier_service.pack('model', clf)
 
 # Save the prediction service to disk for model serving
-saved_path = iris_classifier_service.save()
+saved_path = iris_classifier_service.save("./bentoml")
+
+# saved_path = iris_classifier_service.save_to_dir(path = "./bentoml")
+
+print("종료")
+
+####################save to dir 참고############################ -> codec utf-8 형태로 저장이 안되어 문제가 됨
+# saved_path = iris_classifier_service.save_to_dir()
