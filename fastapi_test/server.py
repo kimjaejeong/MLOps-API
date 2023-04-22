eb9e7877924aa44c3cb4b36fdb7097df3569d957
kimjaejeong <dyddkdlel33@naver.com>
2023-04-22 18:43:36 +0900

    fastapi_test 디렉토리 생성

---
+++ fastapi_test/server.py
@@ -0,0 +1,34 @@
+# from pandas.core.frame import DataFrame
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+import pandas as pd
+from xgboost import XGBClassifier
+
+app = FastAPI()
+
+
+# class Payload(BaseModel):
+#     data : str = ""
+
+@app.get("/")
+def root():
+    return {"Prediction": "Model!!!!"}
+
+
+@app.post("/predict")
+def inference(dict_data: dict = Body(...)):
+    # 모델 불러오기
+    filename = "model/xgb_model.model"
+    new_xgb_model = XGBClassifier()  # 모델 초기화
+    new_xgb_model.load_model(filename)  # 모델 불러오기
+
+    data = dict_data['data']
+    data = pd.DataFrame(data)
+    testX = data.iloc[:, :-1].to_numpy()
+
+    predY = new_xgb_model.predict(testX)
+
+    return predY.tolist()
+
+
+
