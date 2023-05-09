3a2bb326226622f064f54f5a618dd9a39b5dbfa8
kimjaejeong <dyddkdlel33@naver.com>
2023-05-09 23:42:34 +0900

    grpc client 코드 추가

--- grpc_test/client.py
+++ grpc_test/client.py
@@ -12,11 +12,11 @@ def run():
     with grpc.insecure_channel('localhost:50051') as channel:
         # stub을 생성해줍니다.
         stub = helloworld_pb2_grpc.GreeterStub(channel)
-
         # 요청을 보내고 결과를 받는데, 서버에서 지정한 메서드에 요청시 사용할 proto 메시지 형식으로 요청을 전송합니다.
         response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
-    print("Greeter client received: " + response.message)
-
+        print("Greeter client received: " + response.message)
+        response = stub.SayHelloAgain(helloworld_pb2.HelloRequest(name='you'))
+        print("Greeter client received: " + response.message)
 
 if __name__ == '__main__':
     logging.basicConfig()
